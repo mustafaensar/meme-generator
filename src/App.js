41e5a84097c8f8313184f0ce44a7e2046515d074
mustafaensar <mustafaensar55@gmail.com>
2023-03-13 19:49:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Counter from './components/Counter';
+import Form from './components/Form';
+import Meme from './components/Meme';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+import Ternary from './components/Ternary';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/*
+      <Navbar/>
+      <Meme/>
+      <Form/>   
+      <Counter/>   
+    <Signup/>
+    */}
+    <Ternary/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Meme.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Meme(){
+  return(
+    <div>Meme Generator Body</div>
+  )
+}
+
+export default Meme;
